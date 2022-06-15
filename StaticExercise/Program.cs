2c7a161df6419c89f6f373f7708857f3272765cc
David Chuvik <dchuvik@gmail.com>
2022-06-15 11:51:55 -0400

    f

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    //Create a new console app called StaticExercise.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Please input degree's in Celsius to Convert to Fahrenheit: ");
+            Console.WriteLine($"{TempConverter.CelsiusToFahrenheit(Convert.ToDouble(Console.ReadLine()))} degrees Fahrenheit");
+            Console.WriteLine();
+
+            Console.WriteLine($"Please input degree's in Fahrenheit to Convert to Celsius: ");
+            Console.WriteLine($"{TempConverter.FahrenheitToCelsius(Convert.ToDouble(Console.ReadLine()))} degrees Celsius");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    //Now create a static class called TempConverter.The class should have at least 2 methods,
+    //one called FahrenheitToCelsius that will require a double as a parameter and return a double,
+    //the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+    //Fill out these methods and call them in your Program.cs file.Make sure they’re accurate!
+    internal class TempConverter
+    {
+        public TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+           return  (fahrenheit - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
